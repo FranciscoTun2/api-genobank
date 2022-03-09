b41a4da1de65117239c6142a29aafbc435cb9e87
Francisco Tun <francisco.tun@avocadoblock.com>
2022-03-09 10:45:55 -0600

    cherrypy avocado structure

--- start.py
+++ start.py
@@ -3,17 +3,17 @@ import multiprocessing
 from settings import settings
 from run import runweb
 
-server = runweb.GenoBank()
-server.start(port = settings.PORT)
+# server = runweb.GenoBank()
+# server.start(port = settings.PORT)
 
-# def worker(port):
-#     server = runweb.GenoBank()
-#     server.start(port = port)
+def worker(port):
+    server = runweb.GenoBank()
+    server.start(port = port)
 
-# if __name__ == '__main__':
-#     jobs = []
-#     ports = [settings.PORT]
-#     for port in ports:
-#         p = multiprocessing.Process(target=worker, args=(port,))
-#         jobs.append(p)
-#         p.start()
+if __name__ == '__main__':
+    jobs = []
+    ports = [settings.PORT]
+    for port in ports:
+        p = multiprocessing.Process(target=worker, args=(port,))
+        jobs.append(p)
+        p.start()
