56e4bf5083e7fb403d27a0467913f3c6d5c14f99
Francisco Tun <francisco.tun@avocadoblock.com>
2022-03-09 09:59:14 -0600

    first commit

---
+++ runweb.py
@@ -0,0 +1,16 @@
+import cherrypy
+import os
+
+class HelloWorld(object):
+    @cherrypy.expose
+    def index(self):
+        return "Hello World!"
+
+config = {
+    'global': {
+        'server.socket_host': '0.0.0.0',
+        'server.socket_port': int(os.environ.get('PORT', 5000)),
+    }
+}
+
+cherrypy.quickstart(HelloWorld(), '/', config=config)
