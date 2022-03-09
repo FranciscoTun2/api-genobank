bf9c6ea98b5be5c45f0e5cfdbe6da849d75dc905
Francisco Tun <francisco.tun@avocadoblock.com>
2022-03-09 17:53:18 -0600

    added qr lector

--- run/runweb.py
+++ run/runweb.py
@@ -26,33 +26,12 @@ class AppServer(object):
     @cherrypy.tools.allow(methods=['POST'])
     @cherrypy.tools.json_out()
     def read_qrcode(self, file, data):
-        # # Encriptar
-        # sample = "https://genobank.io/certificates/verify-certificate-v1#Daniel%20Francisco%20Uribe%20Benitez%7CG29397810%20MALE%2045%7C2%7CN%7CFM-MDE-89722%7C1642255500000%7C5%7C0x87451c8d50b21f104a8e80f735c774ed223f706532c66b2a8c23980b595b9b412e379373d225d1c1341d96b6d89b3d34dc0483215652281bbd75b97e0fae5c491b%7C1642255576067%7C0x6225a9eacd9ccaec67eca298968f14d98093575f41bd856ffbddd90ded10b17817e122a4fe7a49161eacebf6e060d99182fe92d7d6e122d639f875a3b0c9c2831c%7C0x301068a4e5c335ece530dbb930d62c5d4c96b903fc01b1c03460d15c9f435113"
-        # filename = "sitegenobank.png"
-        # img = qrcode.make(sample)
-        # img.save(filename)
-
-        # # Decriptar
-        # img = cv2.imread("sitewiki.png")
-        # detector = cv2.QRCodeDetector()
-        # data, bbox, straight_qrcode = detector.detectAndDecode(img)
-
-        # print(data)
-
-        print(file)
-        print(data)
-
         file = file.file
         file = file.read()
         # decode binary to image
         img = cv2.imdecode(np.fromstring(file, np.uint8), cv2.IMREAD_COLOR)
         detector = cv2.QRCodeDetector()
         dato, bbox, straight_qrcode = detector.detectAndDecode(img)
-
-        # # # print(file)
-        # print("\nDato", dato, "\n")
-
-
         return dato
 
 class GenoBank(object):
